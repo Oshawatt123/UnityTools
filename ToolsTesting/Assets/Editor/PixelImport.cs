81a7051bdeaae6d3c9df263431a9bc625b6c9ee0
Oshawatt123 <lewis.arnold@hotmail.co.uk>
2020-11-07 01:14:34 +0000

    Pixel Art asset Importer

---
+++ ToolsTesting/Assets/Editor/PixelImport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PixelImport : AssetPostprocessor
+{
+    private void OnPostprocessTexture(Texture2D texture)
+    {
+        TextureImporter textureImporter = (TextureImporter) assetImporter;
+        textureImporter.textureType = TextureImporterType.Sprite;
+        textureImporter.filterMode = FilterMode.Point;
+        textureImporter.textureShape = TextureImporterShape.Texture2D;
+        textureImporter.textureCompression = TextureImporterCompression.Uncompressed;
+    }
+}
