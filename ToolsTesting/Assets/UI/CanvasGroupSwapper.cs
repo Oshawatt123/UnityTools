58eaa51d6cda0436ba6f44f164fa4395a936ad9f
Oshawatt123 <lewis.arnold@hotmail.co.uk>
2020-11-07 02:09:17 +0000

    Added CanvasGroup Swapper & TimeScale to RG Library

---
+++ ToolsTesting/Assets/UI/CanvasGroupSwapper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasGroupSwapper : MonoBehaviour
+{
+    public static void ShowCanvasGroup(CanvasGroup canvasGroup)
+    {
+        canvasGroup.alpha = 1.0f;
+        canvasGroup.interactable = true;
+        canvasGroup.blocksRaycasts = true;
+    }
+
+    public static void HideCanvasGroup(CanvasGroup canvasGroup)
+    {
+        canvasGroup.alpha = 0.0f;
+        canvasGroup.interactable = false;
+        canvasGroup.blocksRaycasts = false;
+    }
+}
